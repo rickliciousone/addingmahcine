fa5344544554d99b2326a4776fa85a715bec4e04
Josh Rychlicki <37146312+rickliciousone@users.noreply.github.com>
2022-04-05 06:15:55 -0400

    Added initial file

---
+++ adding_machine1.py
@@ -0,0 +1,5 @@
+
+first_number = int(input("What is the first number you want to add?"))
+second_number = int(input("What is the second number you want to add?"))
+sum = first_number + second_number
+print("The sum of the numbers is", sum )
