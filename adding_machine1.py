f23b84d347ba5ce53edca940015c664a796f87c6
Josh Rychlicki <37146312+rickliciousone@users.noreply.github.com>
2022-04-05 06:18:12 -0400

    added error checking

---
+++ adding_machine1.py
@@ -0,0 +1,9 @@
+while True:
+    try:
+        first_number = int(input("What is the first number you want to add?"))
+        second_number = int(input("What is the second number you want to add?"))
+        sum = first_number + second_number
+        print("The sum of the numbers is", sum )
+        break
+    except:
+        print("You have provided an invalid input...")
